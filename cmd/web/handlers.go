01cc12da1795ec69886c9ec95d83aa6b8bae75ab
Mathias Bergqvist <mthbqv@gmail.com>
2024-09-02 21:55:23 +0200

    handler refactor

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Hello World"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Show snippet %d", id)
+}
+
+func fooHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Foo handler"))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Create snippet"))
+}
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,44 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
-
-func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-
-	w.Write([]byte("Hello World"))
-}
-
-func showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-
-	fmt.Fprintf(w, "Show snippet %d", id)
-}
-
-func fooHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Foo handler"))
-}
-
-func createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	w.Write([]byte("Create snippet"))
-}
 
 func main() {
 	mux := http.NewServeMux()
