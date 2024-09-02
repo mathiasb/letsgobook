4b9a807626663b2125ef1a1b433c10047f28add2
Mathias Bergqvist <mthbqv@gmail.com>
2024-09-02 21:53:44 +0200

    first structure and refactor

---
+++ cmd/web/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Hello World"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Show snippet %d", id)
+}
+
+func fooHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Foo handler"))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Create snippet"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/foo/", fooHandler)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("Starting server on :3000")
+	err := http.ListenAndServe(":3000", mux)
+
+	log.Fatal(err)
+}
